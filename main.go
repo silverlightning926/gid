2b539a8a4fe2b470b0d7e09232e03487bb39610c
Siddharth Rao <siddrao.926@gmail.com>
2025-07-08 03:05:19 -0700

    Added Status Command

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/sha256"
 	"fmt"
 	"io"
 	"os"
@@ -68,6 +69,23 @@ func copyFile(src, dst string) error {
 	return err
 }
 
+func fileSHA256(filepath string) (string, error) {
+	file, err := os.Open(filepath)
+	if err != nil {
+		return "", fmt.Errorf("failed to open file: %w", err)
+	}
+	defer file.Close()
+
+	hash := sha256.New()
+
+	if _, err := io.Copy(hash, file); err != nil {
+		return "", fmt.Errorf("failed to read file: %w", err)
+	}
+
+	hashBytes := hash.Sum(nil)
+	return fmt.Sprintf("%x", hashBytes), nil
+}
+
 // Cobra
 
 var rootCmd = &cobra.Command{
@@ -76,6 +94,56 @@ var rootCmd = &cobra.Command{
 	Version: version,
 	Short:   "A CLI Tool To Manage Git Profiles",
 	Long:    `gid is a command-line tool for managing multiple Git profiles. It allows you to easily switch between different Git configurations for different projects or contexts.`,
+}
+
+var statusCmd = &cobra.Command{
+	Use:     "status",
+	Aliases: []string{"now"},
+	Short:   "Find the currently set config",
+	Long:    "Find which of the configs has been currently set to active",
+	Run: func(cmd *cobra.Command, args []string) {
+		currentConfigPath, err := getCurrentConfigPath()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		currentConfigPathHash, err := fileSHA256(currentConfigPath)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		availableConfigPaths, err := getAvailableConfigPaths()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		hiBlue := color.New(color.FgHiBlue).SprintFunc()
+		red := color.New(color.FgHiRed).SprintFunc()
+		green := color.New(color.FgGreen).SprintFunc()
+		grey := color.New(color.FgHiBlack).SprintFunc()
+
+		for _, availableConfigPath := range availableConfigPaths {
+
+			availableConfigPathHash, err := fileSHA256(availableConfigPath)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			if currentConfigPathHash == availableConfigPathHash {
+				fileName := filepath.Base(availableConfigPath)
+				profileName := strings.TrimSuffix(fileName, ".gitconfig")
+
+				fmt.Printf("%s %s %s\n", hiBlue("Currently Using Profile"), green(profileName), grey("("+fileName+")"))
+				return
+			}
+		}
+
+		fmt.Printf("%s\n", red("The Current Profile Is Unknown"))
+	},
 }
 
 var listCmd = &cobra.Command{
@@ -116,6 +184,7 @@ var useCmd = &cobra.Command{
 		if err != nil {
 			fmt.Println(err)
 		}
+
 		availableConfigPaths, err := getAvailableConfigPaths()
 		if err != nil {
 			fmt.Println(err)
@@ -142,7 +211,7 @@ var useCmd = &cobra.Command{
 					return
 				}
 
-				fmt.Printf("%s %s %s\n", hiBlue("Using Profile"), green(profileName), grey("("+fileName+")"))
+				fmt.Printf("%s %s %s\n", hiBlue("Switched To Using Profile"), green(profileName), grey("("+fileName+")"))
 				return
 			}
 		}
@@ -153,6 +222,7 @@ var useCmd = &cobra.Command{
 }
 
 func init() {
+	rootCmd.AddCommand(statusCmd)
 	rootCmd.AddCommand(listCmd)
 	rootCmd.AddCommand(useCmd)
 }
